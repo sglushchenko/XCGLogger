26a385bcb1d3a58699cf90b82285ca70d80ec35a
Sergey Glushchenko <glushchenko2003@gmail.com>
2023-06-20 21:07:12 +0800

    set Package.swift version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -30,5 +30,6 @@ let package = Package(
         .target(
             name: "ObjcExceptionBridging",
             dependencies: []),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
